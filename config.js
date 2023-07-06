3640b5bef19dbbef8c69e86b934adb0027fe7bc9
IzansStore <115550578+IzansStore@users.noreply.github.com>
2023-07-06 23:34:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,25 +6,25 @@ const readMore = more.repeat(4001)
 
 // Owner
 global.owner = [
-['6285760451683', 'Zeltoria', true]
+['6282298627146', 'IzandXD', true]
 ]
 global.mods = []
 global.prems = []
 // Info
-global.nomorwa = '6285760451683'
-global.author = '© Xyroinee'
+global.nomorwa = '6282298627146'
+global.author = '© IzansXD'
 global.namebot = '​Clara - MD'
-global.wm = '© Clara By Xyroine'
+global.wm = '© Clara By IzansXD'
 global.stickpack = 'Clara - MD'
-global.stickauth = '© Xyroine'
+global.stickauth = '© IzansXD'
 // Link Sosmed
-global.sig = 'https://www.instagram.com/danilelistz02'
-global.sgh = 'https://github.com/Xyroinee'
-global.sgc = 'https://chat.whatsapp.com/LAWdMIQri9SKee5kqmISH0'
+global.sig = 'https://www.instagram.com/AfkLeeOfc'
+global.sgh = 'https://github.com/IzansStore'
+global.sgc = 'https://chat.whatsapp.com/EZz7IuEJnHX04TqqeRQm6V'
 // Donasi
-global.psaweria = 'https://saweria.co/Zeltoria'
-global.ptrakterr = 'https://trakteer.id/zeltoria'
-global.povo = '0896-1075-0607'
+global.psaweria = 'https://saweria.co/HinataMd'
+global.ptrakterr = '-'
+global.povo = 0822-9862-7146'
 // Info Wait
 global.wait = '_Sedang Di Proses, Mohon Tunggu_....'
 global.eror = 'Terjadi Kesalahan Coba Lagi Nanti!'
